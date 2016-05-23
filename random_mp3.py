eb599c5513c432756be5135fbdeb072bbde70e2b
Fabricio Toresan <fabricio.toresan@gmail.com>
2016-05-23 19:55:09 -0300

    Improved args parsing with argparse

--- random_mp3.py
+++ random_mp3.py
@@ -1,28 +1,40 @@
-import sys, os, random, glob, shutil
+import sys, os, random, glob, shutil, argparse
 
 def copyFiles():
-	if len(sys.argv) < 3:
-		print('Insufficient arguments!\n')
-		print('Usage: python3 random_mp3 <source_dir> <dest_dir> [limit (Mb)]\n')
-		sys.exit(0)															
-	source = sys.argv[1]
+
+	parser = argparse.ArgumentParser(description='Copy random mp3s files')
+	parser.add_argument('source', metavar='source', help='Folder where the mp3 files must be copied from')
+	parser.add_argument('dest', metavar='dest', help='Destination folder where the mp3 files must be copied to')
+	parser.add_argument('limit', metavar='limit', type=int, help='Size limit to copy (in Mb)')
+	parser.add_argument('-v', dest='verbose', required=False, action='store_true', help='verbose mode')
+
+	args = parser.parse_args()
+	source = args.source
 	source += '**/*.mp3'
-	dest = sys.argv[2]
-	limit = sys.argv[3]
-	limit = int(limit)
+	dest = args.dest
+	limit = args.limit
 	limit *= 1024 * 1024
 	size = 0
 	allFiles = glob.glob(source, recursive=True)
 	random.shuffle(allFiles) # Extra shuffling
-	while size < limit:
+	while size < limit and len(allFiles) > 0:
 		chosen = random.choice(allFiles)
 		try:
 			shutil.copy(chosen, dest)
 			size += os.stat(chosen).st_size
 		except:
-			print ('Error copying ' + chosen) 		
-		sys.stdout.write("Copying %d Mb of %d Mb \r" % ((size / 1024 / 1024), (limit / 1024 / 1024)) )
-		sys.stdout.flush()
+			try: 
+				print ('Error copying ' + chosen)
+			except:
+ 				print ('You have a song with a messy name!')
+		if args.verbose:
+			print("Copying " + chosen + "\n\t(" + str(int(size / 1024 / 1024)) + " Mb of " + str(limit / 1024 / 1024) + " Mb)")
+		else:
+			sys.stdout.write("Copying %d Mb of %d Mb \r" % ((size / 1024 / 1024), (limit / 1024 / 1024)) )
+			sys.stdout.flush()
+		allFiles.remove(chosen)
 
+	if size < limit:
+		print("Insufficient files to reach the specified limit")
 if __name__ == "__main__":
 	copyFiles()
